32adf6c43e17db9d880a515811349e013cb0b630
TehKluma <tehkluma@users.noreply.github.com>
2026-09-16 14:47:26 -0400

    feat: bundle CEF resources, enable obs_portable_mode & add diagnostic launcher alerts

--- launcher/launcher.cpp
+++ launcher/launcher.cpp
@@ -32,11 +32,25 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     // Launch with correct working directory (bin/64bit)
     if (CreateProcessW(targetExe.c_str(), GetCommandLineW(), NULL, NULL, FALSE, 0, NULL, workDir.c_str(), &si, &pi)) {
+        // Wait up to 2 seconds to see if it immediately crashes or fails initialization
+        DWORD waitResult = WaitForSingleObject(pi.hProcess, 2000);
+        if (waitResult == WAIT_OBJECT_0) {
+            DWORD exitCode = 0;
+            GetExitCodeProcess(pi.hProcess, &exitCode);
+            if (exitCode != 0) {
+                std::wstring msg = L"Velora Studio engine exited with code: 0x" + std::to_wstring(exitCode) +
+                                   L"\n\nPlease verify that graphics drivers and DirectX 11+ runtime are up to date.";
+                MessageBoxW(NULL, msg.c_str(), L"Velora Studio - Startup Error", MB_OK | MB_ICONERROR);
+            }
+        }
         CloseHandle(pi.hProcess);
         CloseHandle(pi.hThread);
         return 0;
     }
 
-    MessageBoxW(NULL, L"Failed to start Velora Studio engine from bin\\64bit.\nPlease ensure all files are extracted.", L"Velora Studio", MB_OK | MB_ICONERROR);
+    DWORD err = GetLastError();
+    std::wstring errMsg = L"Failed to start Velora Studio engine from bin\\64bit (Error: " + std::to_wstring(err) +
+                          L").\nPlease ensure all package files are fully extracted.";
+    MessageBoxW(NULL, errMsg.c_str(), L"Velora Studio", MB_OK | MB_ICONERROR);
     return 1;
 }
